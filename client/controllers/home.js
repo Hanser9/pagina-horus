54834fe8a9de998d6bab87721f8535f481ff5c77
carlos <carlos@gabssa.net>
2019-01-16 19:00:08 -0600

    correo

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,5 +1,6 @@
 require('../services/home');
 var alertify = require('alertifyjs');
+var nodemailer     = require('nodemailer');
 
 angular.module(MODULE_NAME)
 .controller('homeCtrl', ['$scope', 'HomeService', '$timeout', function($scope, HomeService, $timeout) {
@@ -30,18 +31,56 @@ angular.module(MODULE_NAME)
       if (validar_email(correo.mail)) {
         console.log('entro');
         var d = correo
-        HomeService.enviarMail(d)
-        .success(function(res){
-          console.log(res);
-          alertify.success('Correo Enviado');
-          $scope.correo = {}
-          $scope.loading = false;
-        })
+        enviarMail(d)
       }else {
           alertify.error('Ingrese un correo valido');
           $scope.loading = false;
       }
     }
+  }
+
+  function enviarMail(d){
+      console.log(d);
+      var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+         user: 'josehdez40@gmail.com',
+         pass: 'homer213'
+        }
+      });
+
+      var correo = `
+                    <p>Contacto de la pagina de Horus</p>
+                    <h3>Detalles de contacto</h3>
+                    <ul>
+                      <li>Nombre: ${d.nombre}</li>
+                      <li>Correo: ${d.mail}</li>
+                      <li>Telefono: ${d.tel}</li>
+                    </ul>
+                    <h3>Mensaje</h3>
+                    <p>${d.mensaje}</p>
+                  `
+
+      var message = {
+            from: '"Contacto de Horus"<josehdez40@gmail.com>',
+            to: 'jcarlos.horus@gmail.com',
+            subject: 'Contacto Horus Network',
+            html: correo
+        };
+
+        transporter.sendMail(message, function(err) {
+          if (!err) {
+            console.log('Email enviado');
+            alertify.success('Correo Enviado');
+            $scope.correo = {}
+            $scope.loading = false;
+          } else
+            console.log(err);
+            alertify.error('Error al enviar el correo, intente mas tarde');
+            $scope.loading = false;
+        });
   }
 
 
